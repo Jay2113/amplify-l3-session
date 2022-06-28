256432d7ca08dcecf7c35de807150b257cce2930
Raval <jayrava@88665a48afb0.ant.amazon.com>
2022-06-28 08:09:38 -0700

    modified app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World from test!
+          Hello World from QA!
         </a>
       </header>
     </div>
