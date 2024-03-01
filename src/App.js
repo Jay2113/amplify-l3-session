11e7aa4cf63bedb9727320b4a329612b2b886ef3
Jay Raval <jay.ambrish.raval@gmail.com>
2024-03-01 11:14:24 -0500

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing GitHub Connectivity!
+          Testing GitHub Connectivity - AWS Amplify!
         </a>
       </header>
     </div>
